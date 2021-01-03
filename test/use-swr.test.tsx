c2d38b53a1270df9d7f3f092add68dc4e10b91a6
Yixuan Xu <yixuanxu94@outlook.com>
2021-01-03 15:56:34 +0800

    check key before calling callback

--- test/use-swr.test.tsx
+++ test/use-swr.test.tsx
@@ -844,6 +844,66 @@ describe('useSWR - revalidate', () => {
     })
     expect(container.firstChild.textContent).toMatchInlineSnapshot(`"false"`)
   })
+
+  it('the previous interval timer should not call onSuccess callback if key changes too fast', async () => {
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    }
+    const fetcherWithToken = jest.fn(async token => {
+      await sleep(200)
+      return token
+    })
+    const onSuccess = jest.fn((data, key) => {
+      return `${data} ${key}`
+    })
+    function Page() {
+      const [count, setCount] = useState(0)
+      const { data } = useSWR(count.toString(), fetcherWithToken, {
+        refreshInterval: 100,
+        dedupingInterval: 50,
+        onSuccess
+      })
+      return (
+        <button
+          onClick={() => setCount(count + 1)}
+        >{`click me ${data}`}</button>
+      )
+    }
+    const { container } = render(<Page />)
+
+    // initial revalidate
+    await act(() => sleep(200))
+    expect(fetcherWithToken).toBeCalledTimes(1)
+    expect(onSuccess).toBeCalledTimes(1)
+    expect(onSuccess).toHaveLastReturnedWith(`0 0`)
+    // first refresh
+    await act(() => sleep(100))
+    expect(fetcherWithToken).toBeCalledTimes(2)
+    expect(fetcherWithToken).toHaveBeenLastCalledWith('0')
+    await act(() => sleep(200))
+    expect(onSuccess).toBeCalledTimes(2)
+    expect(onSuccess).toHaveLastReturnedWith(`0 0`)
+
+    // second refresh start
+    await act(() => sleep(100))
+    expect(fetcherWithToken).toBeCalledTimes(3)
+    expect(fetcherWithToken).toHaveBeenLastCalledWith('0')
+    // change the key during revalidation
+    // The second refresh will not start a new timer
+    fireEvent.click(container.firstElementChild)
+
+    // first refresh with new key 1
+    await act(() => sleep(100))
+    expect(fetcherWithToken).toBeCalledTimes(4)
+    expect(fetcherWithToken).toHaveBeenLastCalledWith('1')
+    await act(() => sleep(210))
+    expect(onSuccess).toBeCalledTimes(3)
+    expect(onSuccess).toHaveLastReturnedWith(`1 1`)
+
+    // second refresh with new key 1
+    expect(fetcherWithToken).toBeCalledTimes(5)
+    expect(fetcherWithToken).toHaveBeenLastCalledWith('1')
+  })
 })
 
 describe('useSWR - error', () => {
