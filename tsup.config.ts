52c44708971b0d5bfef4acc4597c25b9ed2b219a
Yixuan Xu <yixuanxu94@outlook.com>
2023-01-27 21:16:14 +0800

    remove package.json

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'core/index.ts',
+    'immutable/index.ts',
+    'infinite/index.ts',
+    'mutation/index.ts',
+    '_internal/index.ts'
+  ],
+  format: ['cjs', 'esm'],
+  target: 'es2018',
+  clean: true,
+  dts: true,
+  external: ['swr', 'swr/_internal', 'react']
+})
